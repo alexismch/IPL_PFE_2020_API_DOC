6efe438a65eba7a20c03d9282f60b113e013bd4a
Mathieu COSYNS <mathieu.cosyns@student.vinci.be>
2020-12-05 23:21:23 +0100

    init self-hosted doc

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./DOC-API.yaml");
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
